d4a9909417f097111960f11defd2b7abd5d2f031
viken <viken2011@gmail.com>
2019-12-04 14:21:44 +0800

    修改build脚本

--- php-5.6-alpine/build.sh
+++ php-5.6-alpine/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build -t php56-alpine -f ./Dockerfile .
+sudo docker build -t php56-alpine:v1.0 -f ./Dockerfile .
--- php-7.3-alpine/build.sh
+++ php-7.3-alpine/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker build -t php73-alpine -f ./Dockerfile .
+docker build . --file Dockerfile --tag php73-alpine:v1.0
